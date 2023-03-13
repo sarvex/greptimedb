4ce98fd9ec4b9778af1bd57b51bb2700f20b4fe5
evenyag <realevenyag@gmail.com>
2023-03-13 12:18:20 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@296c6dfcbff1a4a43de05cdbc29d54a16c4045d4 🚀

--- datanode/sql/sidebar-items.js
+++ datanode/sql/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SqlRequest"],"mod":["alter","copy_table","copy_table_from","create","delete","drop_table","insert"],"opaque":[""],"static":["CALLSITE","META"],"struct":["SqlHandler"]};
+window.SIDEBAR_ITEMS = {"enum":["SqlRequest"],"mod":["alter","copy_table","copy_table_from","create","delete","drop_table","flush_table","insert"],"opaque":[""],"static":["CALLSITE","META"],"struct":["SqlHandler"]};
