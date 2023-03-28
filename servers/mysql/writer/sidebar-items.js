2affbd3ec1064460c64e7e7c8d05e3a19575f74a
MichaelScofield <bayinamine@gmail.com>
2023-03-28 06:42:08 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@47179a7812dd365f9a4665ead1bf8695500d02d4 🚀

--- servers/mysql/writer/sidebar-items.js
+++ servers/mysql/writer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["create_mysql_column","create_mysql_column_def"],"opaque":[""],"static":["CALLSITE","META"],"struct":["MysqlResultWriter","QueryResult"]};
+window.SIDEBAR_ITEMS = {"fn":["create_mysql_column","create_mysql_column_def","write_output"],"opaque":[""],"static":["CALLSITE","META"],"struct":["MysqlResultWriter","QueryResult"]};
