302a26a0892c1de376207395179c31ea68421a17
evenyag <realevenyag@gmail.com>
2023-03-29 09:25:24 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@d7cadf6e6d321bcd302aea9df4bc904c7f03f042 🚀

--- storage/compaction/task/sidebar-items.js
+++ storage/compaction/task/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"opaque":[""],"static":["CALLSITE","META"],"struct":["CompactionOutput","CompactionTaskImpl"],"trait":["CompactionTask"]};
+window.SIDEBAR_ITEMS = {"constant":["MAX_PARALLEL_COMPACTION"],"opaque":[""],"static":["CALLSITE","META"],"struct":["CompactionOutput","CompactionTaskImpl"],"trait":["CompactionTask"]};
