4a1e4cf44c31ac2bf50935bcf2ff05e2323014ca
sunng87 <classicning@gmail.com>
2023-03-31 02:09:35 +0000

    Deploying to gh-pages from @ GreptimeTeam/greptimedb@fae293310ca12f096d46f26c3c4d3233eef3fb1a 🚀

--- implementors/catalog/trait.CatalogList.js
+++ implementors/catalog/trait.CatalogList.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "catalog":[],
-"frontend":[["impl CatalogList for <a class=\"struct\" href=\"frontend/catalog/struct.FrontendCatalogManager.html\" title=\"struct frontend::catalog::FrontendCatalogManager\">FrontendCatalogManager</a>"]]
+"frontend":[["impl <a class=\"trait\" href=\"catalog/trait.CatalogList.html\" title=\"trait catalog::CatalogList\">CatalogList</a> for <a class=\"struct\" href=\"frontend/catalog/struct.FrontendCatalogManager.html\" title=\"struct frontend::catalog::FrontendCatalogManager\">FrontendCatalogManager</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
